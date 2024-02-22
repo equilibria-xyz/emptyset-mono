95b689cd2863ed14a11d689b5194c3233f78a51b
Kevin Britz <kevin@equilibria.xyz>
2024-02-21 19:54:01 -0800

    add migration reserve

--- packages/emptyset-reserve/contracts/reserve/MigrationReserve.sol
+++ packages/emptyset-reserve/contracts/reserve/MigrationReserve.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.17;
 
 import "@equilibria/root/number/types/UFixed18.sol";
+import "../interfaces/IMigrationReserve.sol";
 import "./SimpleReserve.sol";
 
-contract MigrationReserve is SimpleReserve {
+contract MigrationReserve is IMigrationReserve, SimpleReserve {
     Token6 public immutable USDC_BRIDGED; // solhint-disable-line var-name-mixedcase
 
     constructor(Token18 dsu_, Token6 usdc_, Token6 usdcBridged_) SimpleReserve(dsu_, usdc_) {
@@ -12,7 +13,9 @@ contract MigrationReserve is SimpleReserve {
     }
 
     function migrate(UFixed18 amount) external {
-        USDC.pull(msg.sender, amount, true);
+        USDC.pull(msg.sender, amount);
         USDC_BRIDGED.push(msg.sender, amount);
+
+        emit Migrate(msg.sender, amount);
     }
 }
