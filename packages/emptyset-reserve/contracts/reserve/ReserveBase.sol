76da03069d075b736affabe4e8c2ce6c8e48014d
Kevin Britz <kevin@equilibria.xyz>
2024-05-26 21:22:18 -0700

    add compound market

--- packages/emptyset-reserve/contracts/reserve/ReserveBase.sol
+++ packages/emptyset-reserve/contracts/reserve/ReserveBase.sol
@@ -3,21 +3,15 @@ pragma solidity 0.8.17;
 
 import { DSU as IDSU } from "@emptyset/dsu/contracts/DSU.sol";
 import { Token18 } from "@equilibria/root/token/types/Token18.sol";
-import { Token6 } from "@equilibria/root/token/types/Token6.sol";
-import { UFixed6, UFixed6Lib } from "@equilibria/root/number/types/UFixed6.sol";
 import { UFixed18, UFixed18Lib } from "@equilibria/root/number/types/UFixed18.sol";
 import { Initializable } from "@equilibria/root/attribute/Initializable.sol";
 import { IReserve } from "../interfaces/IReserve.sol";
 
-contract ReserveBase is IReserve, Initializable {
+abstract contract ReserveBase is IReserve, Initializable {
     Token18 public immutable DSU; // solhint-disable-line var-name-mixedcase
-    Token6 public immutable USDC; // solhint-disable-line var-name-mixedcase
-    ICompoundV3USDC public immutable COMPOUND; // solhint-disable-line var-name-mixedcase
 
-    constructor(Token18 dsu_, Token6 usdc_, ICompoundV3USDC compound_) {
+    constructor(Token18 dsu_) {
         DSU = dsu_;
-        USDC = usdc_;
-        COMPOUND = compound_;
     }
 
     function initialize() external initializer(1) {
@@ -62,33 +56,10 @@ contract ReserveBase is IReserve, Initializable {
         emit Redeem(msg.sender, amount, redemptionAmount);
     }
 
-    function _pull(UFixed18 amount) internal {
-        USDC.pull(msg.sender, UFixed6Lib.from(amount, true));
-    }
-
-    function _push(UFixed18 amount) internal {
-        USDC.push(msg.sender, UFixed6Lib.from(amount));
-    }
-
-    function _collateral() internal view returns (UFixed18) {
-        return UFixed18Lib.from(COMPOUND.balanceOf(address(this)));
-    }
-
-    function _assets() internal view returns (UFixed18) {
-        return UFixed18Lib.from(COMPOUND.balanceOf(address(this)));
-    }
-
-    function _deposit(UFixed18 amount) internal {
-        COMPOUND.supply(USDC, UFixed6Lib.from(amount, true));
-    }
-
-    function _withdraw(UFixed18 amount) internal {
-        COMPOUND.withdraw(USDC, UFixed6Lib.from(amount));
-    }
-}
-
-interface ICompoundV3USDC {
-    function supply(Token6 asset, UFixed6 amount) external;
-    function withdraw(Token6 asset, UFixed6 amount) external;
-    function balanceOf(address account) external view returns (UFixed6);
+    function _pull(UFixed18 amount) internal virtual;
+    function _push(UFixed18 amount) internal virtual;
+    function _collateral() internal virtual view returns (UFixed18);
+    function _assets() internal virtual view returns (UFixed18);
+    function _deposit(UFixed18 amount) internal virtual;
+    function _withdraw(UFixed18 amount) internal virtual;
 }
---
+++ packages/emptyset-reserve/contracts/reserve/impl/CompoundV3USDC.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.17;
+
+import { Token18 } from "@equilibria/root/token/types/Token18.sol";
+import { Token6 } from "@equilibria/root/token/types/Token6.sol";
+import { UFixed6, UFixed6Lib } from "@equilibria/root/number/types/UFixed6.sol";
+import { UFixed18, UFixed18Lib } from "@equilibria/root/number/types/UFixed18.sol";
+import { ReserveBase } from "../ReserveBase.sol";
+
+contract CompoundV3USDC is ReserveBase {
+    Token6 public immutable USDC; // solhint-disable-line var-name-mixedcase
+    ICompoundV3USDC public immutable COMPOUND; // solhint-disable-line var-name-mixedcase
+
+    constructor(Token18 dsu_, Token6 usdc_, ICompoundV3USDC compound_) ReserveBase(dsu_) {
+        USDC = usdc_;
+        COMPOUND = compound_;
+    }
+
+    function _pull(UFixed18 amount) internal override {
+        USDC.pull(msg.sender, UFixed6Lib.from(amount, true));
+    }
+
+    function _push(UFixed18 amount) internal override {
+        USDC.push(msg.sender, UFixed6Lib.from(amount));
+    }
+
+    function _collateral() internal override view returns (UFixed18) {
+        return UFixed18Lib.from(COMPOUND.balanceOf(address(this)));
+    }
+
+    function _assets() internal override view returns (UFixed18) {
+        return UFixed18Lib.from(COMPOUND.balanceOf(address(this)));
+    }
+
+    function _deposit(UFixed18 amount) internal override {
+        COMPOUND.supply(USDC, UFixed6Lib.from(amount, true));
+    }
+
+    function _withdraw(UFixed18 amount) internal override {
+        COMPOUND.withdraw(USDC, UFixed6Lib.from(amount));
+    }
+}
+
+interface ICompoundV3USDC {
+    function supply(Token6 asset, UFixed6 amount) external;
+    function withdraw(Token6 asset, UFixed6 amount) external;
+    function balanceOf(address account) external view returns (UFixed6);
+}
